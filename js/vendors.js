38ac61b8d728f944d26680cdf732ee973186f974
Sara Hedbrandh Strömberg <sarahedbrandh@gmail.com>
2018-10-10 15:55:12 +0200

    Smaller fixes in vendor.js

--- js/vendors.js
+++ js/vendors.js
@@ -2,13 +2,15 @@
 // TODO: Show vendors() Lista slide up.
 var showVendorsText = document.querySelector("#vendor-list-title");
 var vendorElement = document.querySelector("#vendor-list-wrapper");
+var vendorList = document.querySelector("#vendor-list-vendors");
 
 showVendorsText.addEventListener('click', showVendors, false);
 
 
 
 function showVendors(){ 
-    vendorElement.style.top = "100px";
+    vendorElement.style.top = "500px"; //TODO: calc hur långt upp ska den
+    
     showVendorsText.removeEventListener('click', showVendors, false);
     showVendorsText.addEventListener('click', hideVendors, false);
     
@@ -19,6 +21,7 @@ function hideVendors() {
     showVendorsText.removeEventListener('click', hideVendors, false);
     showVendorsText.addEventListener('click', showVendors, false);
     vendorElement.style.top = "calc(100% - 100px)";
+    vendorList.style.overflow = "hidden";
 }
 
 
