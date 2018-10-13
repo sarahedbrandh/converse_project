c3ca37fa251c236169d73ab59ff1aac5d77e97c5
Sara Hedbrandh Strömberg <sarahedbrandh@gmail.com>
2018-10-13 10:36:46 +0200

    function for changing the background in navbar added

--- js/navbar.js
+++ js/navbar.js
@@ -17,3 +17,25 @@ function navToggle() {
 //TODO: showContent(event)    visability: visable  hämta div med motsvarande id och gör den visable
 
 //TODO: enlargeLogo() när man hovrar över logan så ska loggan bli större
+
+
+//TODO: make background <-> transparent white when click on navbar
+
+
+
+
+// ===============  Changing the navbar-background to white when clicking ============== S//
+
+var menuItems = document.querySelectorAll(".menu-items");
+
+menuItems.forEach(function(item){
+    item.addEventListener('click', changeNav, false);
+    console.log(item);
+});
+
+function changeNav(event) {
+    var topNav = document.querySelector(".topnav");
+    console.log(event.target.id);
+    topNav.style.backgroundColor = "white";
+}
+
