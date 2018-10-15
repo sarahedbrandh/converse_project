d82f532b8e89696eff6f72faa16fc5543faecf48
Oscar Lindström <ogge@upldesign.se>
2018-10-15 08:39:45 +0200

    js start

--- js/navbar.js
+++ js/navbar.js
@@ -10,9 +10,31 @@ function navToggle() {
 //TODO: menuClick(event) -> dölja alla sectioner .section-content  (hideContent())
 //                       -> visa innehåll för aktuell .section-content #content-?   (showContent(event))
 
-document.querySelector('.menu-items')
+    var menuItems = document.querySelectorAll('.menu-items');
+    console.log(menuItems);
+    menuItems.forEach(function(item){
+        console.log("testin");
+    });
+    console.log('test');
 
-function menuClick(EventTarget)
+
+
+
+ //har valt menyalternativen
+
+// STEG 2 Om man klickar visa den item 
+
+// steg 3 Dölj nuvarande sektion 
+
+function menuClick(EventTarget){
+    document.querySelectorAll('.section-content').forEach(function(content) {
+        content.style.visibility = "hidden" ;
+
+        }
+        )
+}
+
+
 
 //TODO: hideContent()         hämta alla divar och sätt dem till hidden
 //TODO: showContent(event)    visability: visable  hämta div med motsvarande id och gör den visable
