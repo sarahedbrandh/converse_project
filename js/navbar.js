b32a1a31fde0ac61f829579a318831173d0a8b57
Sara Hedbrandh Strömberg <sarahedbrandh@gmail.com>
2018-10-15 14:47:36 +0200

    navbar link bugg fixed

--- js/navbar.js
+++ js/navbar.js
@@ -1,6 +1,6 @@
 function navToggle() {
     var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.classList.contains("topnav")) {
         x.classList.add("dropped");
     } else {
         x.classList.remove("dropped");
@@ -8,18 +8,6 @@ function navToggle() {
 }
 
 
-
-//
-//TODO: menuClick(event) -> dölja alla sectioner .section-content  (hideContent())
-//                       -> visa innehåll för aktuell .section-content #content-?   (showContent(event))
-
-//TODO: hideContent()         hämta alla divar och sätt dem till hidden
-//TODO: showContent(event)    visability: visable  hämta div med motsvarande id och gör den visable
-
-//TODO: enlargeLogo() när man hovrar över logan så ska loggan bli större
-
-
-//TODO: make background <-> transparent white when click on navbar
 
 
 
